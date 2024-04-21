de1bcca0e5bb637d3a00207aa25e83fc1f425cf4
Hamza <82061179+hxxmz@users.noreply.github.com>
2024-04-21 21:43:13 +0500

    20240421
    Trying to add config file for toggle memo (bugs)

--- toggable_badge_after_spawn.lua
+++ toggable_badge_after_spawn.lua
@@ -1,20 +1,58 @@
+script_name("toggable_badge_after_spawn")
+script_author("Hyam - Inspired by Bear")
+script_version("1.0.0")
+
 require "moonloader"
 require "sampfuncs"
-local sampev = require "lib.samp.events"
 
-script_name("toggable_badge_after_spawn")
+local sampev = require "lib.samp.events"
+local inicfg = require "inicfg"
+
+-----------------------------------------------------
+-- CONFIG
+-----------------------------------------------------
+
+local config_dir_path = getWorkingDirectory() .. "\\config\\"
+if not doesDirectoryExist(config_dir_path) then createDirectory(config_dir_path) end
+
+local config_file_path = config_dir_path .. "BadgeAfterSpawnHyam.ini"
+
+config_dir_path = nil
+
+local config_table
+
+if doesFileExist(config_file_path) then
+	config_table = inicfg.load(nil, config_file_path)
+else
+	local new_config = io.open(config_file_path, "w")
+	new_config:close()
+	new_config = nil
+	
+	config_table = {Options = {isBadgeAfterSpawnEnabled = true}}
+
+	if not inicfg.save(config_table, config_file_path) then
+		sampAddChatMessage("--- {FFFFFF}Badge after Spawn by {AAAAFF}Hyam{FFFFFF}: Config file creation failed - contact the developer for help.", -1)
+		
+	end
+end
+
+-----------------------------------------------------
+-- MAIN
+-----------------------------------------------------
 
 function main()
 	if not isSampLoaded() or not isSampfuncsLoaded() then return end
 	while not isSampAvailable() do wait(100) end
-	sampAddChatMessage("--- {FFFFFF}Badge after Spawn (Toggable) by {AAAAFF}Hyam {FFFFFF}| Use {AAAAFF}/togbas {FFFFFF}to toggle.", -1)
+	sampAddChatMessage("--- {FFFFFF}Badge after Spawn (Toggable) v" .. script.this.version .. "by {AAAAFF}Hyam {FFFFFF}| Use {AAAAFF}/togbas {FFFFFF}to toggle.", -1)
 	sampRegisterChatCommand("togbas", cmd)
 	while true do
 		wait(0)
 	end
 end
 
-local toggle = false
+-----------------------------------------------------
+-- API-SPECIFIC FUNCTIONS
+-----------------------------------------------------
 
 function sampev.onServerMessage(c, text)
 	local result, msg = string.find(text, "Your hospital bill was paid for by your faction insurance.")
@@ -23,11 +61,24 @@ function sampev.onServerMessage(c, text)
 	end
 end
 
-function cmd()
-	toggle = not toggle
-	if not toggle then
-		sampAddChatMessage("*** {FFFFFF}Badge After Spawn: {AAAAFF}Off", -1)
+-----------------------------------------------------
+-- LOCALLY DECLARED FUNCTIONS
+-----------------------------------------------------
+
+-- local toggle = false
+
+local function cmd()
+	if config_table.Options.isBadgeAfterSpawnEnabled then
+		config_table.Options.isBadgeAfterSpawnEnabled = false
+		if inicfg.save(config_table, config_file_path) then
+			sampAddChatMessage("*** {FFFFFF}Badge After Spawn: {AAAAFF}Off", -1)
+		else
+			sampAddChatMessage("--- {AAAAFF}Badge After Spawn: {FFFFFF}Badge toggle in config failed - contact the developer for help.", -1)
 	else 
-		sampAddChatMessage("*** {FFFFFF}Badge After Spawn: {AAAAFF}On", -1)
+		config_table.Options.isBadgeAfterSpawnEnabled = true
+		if inicfg.save(config_table, config_file_path) then
+			sampAddChatMessage("*** {FFFFFF}Badge After Spawn: {AAAAFF}On", -1)
+		else
+			sampAddChatMessage("--- {AAAAFF}Badge After Spawn: {FFFFFF}Badge toggle in config failed - contact the developer for help.", -1)
 	end
 end
