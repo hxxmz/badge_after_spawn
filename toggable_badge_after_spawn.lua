4f6b94afc659960746f9735f8e68def46869fbce
Hamza <82061179+hxxmz@users.noreply.github.com>
2024-12-17 11:15:52 +0500

    Update toggable_badge_after_spawn.lua

--- toggable_badge_after_spawn.lua
+++ toggable_badge_after_spawn.lua
@@ -17,36 +17,39 @@ if not doesDirectoryExist(config_dir_path) then createDirectory(config_dir_path)
 
 local config_file_path = config_dir_path .. "BadgeAfterSpawnHyam.ini"
 
-config_dir_path = nil
+local default_config = {
+    Options = {
+        isBadgeAfterSpawnEnabled = true
+    }
+}
 
-local config_table
+local config_table = nil
 
 if doesFileExist(config_file_path) then
-	config_table = inicfg.load(nil, config_file_path)
+    config_table = inicfg.load(default_config, config_file_path)
 else
-	local new_config = io.open(config_file_path, "w+")
-	if new_config then
-		new_config:write("Options = {\nisBadgeAfterSpawnEnabled = true\n}")
-		new_config:close()
-		config_table = inicfg.load(nil, config_file_path)
-  	else
-		sampAddChatMessage("--- {FFFFFF}Badge after Spawn by {AAAAFF}Hyam{FFFFFF}: Config file creation failed - contact the developer for help.", -1)
-  	end
+    local success = inicfg.save(default_config, config_file_path)
+    if success then
+        config_table = default_config
+    else
+        sampAddChatMessage("--- {FFFFFF}Badge after Spawn by {AAAAFF}Hyam{FFFFFF}: Config file creation failed - contact the developer for help.", -1)
+    end
 end
-
 
 -----------------------------------------------------
 -- MAIN
 -----------------------------------------------------
 
 function main()
-	if not isSampLoaded() or not isSampfuncsLoaded() then return end
-	while not isSampAvailable() do wait(100) end
-	sampAddChatMessage("--- {FFFFFF}Badge after Spawn (Toggable) v" .. script.this.version .. "by {AAAAFF}Hyam {FFFFFF}| Use {AAAAFF}/togbas {FFFFFF}to toggle.", -1)
-	sampRegisterChatCommand("togbas", cmd)
-	while true do
-		wait(0)
-	end
+    if not isSampLoaded() or not isSampfuncsLoaded() then return end
+    while not isSampAvailable() do wait(100) end
+
+    sampAddChatMessage("--- {FFFFFF}Badge after Spawn (Toggable) v" .. script.this.version .. " by {AAAAFF}Hyam {FFFFFF}| Use {AAAAFF}/bas {FFFFFF}to toggle.", -1)
+    sampRegisterChatCommand("bas", cmd)
+
+    while true do
+        wait(0)
+    end
 end
 
 -----------------------------------------------------
@@ -54,24 +57,27 @@ end
 -----------------------------------------------------
 
 function sampev.onServerMessage(c, text)
-	local result, msg = string.find(text, "Your hospital bill was paid for by your faction insurance.")
-	if result and config_table.Options.isBadgeAfterSpawnEnabled then
-		sampSendChat("/badge")
-	end
+    local result = string.find(text, "Your hospital bill was paid for by your faction insurance.")
+    if result and config_table.Options.isBadgeAfterSpawnEnabled then
+        sampSendChat("/badge")
+    end
 end
 
 -----------------------------------------------------
 -- LOCALLY DECLARED FUNCTIONS
 -----------------------------------------------------
 
-local function cmd()
-    
-	config_table.Options.isBadgeAfterSpawnEnabled = not config_table.Options.isBadgeAfterSpawnEnabled
-    
-    if inicfg.save(config_table, config_file_path) then
-        local status = config_table.Options.isBadgeAfterSpawnEnabled and "On" or "Off"
-        sampAddChatMessage("*** {FFFFFF}Badge After Spawn: {AAAAFF}" .. status, -1)
+function cmd()
+    if config_table then
+        config_table.Options.isBadgeAfterSpawnEnabled = not config_table.Options.isBadgeAfterSpawnEnabled
+
+        if inicfg.save(config_table, config_file_path) then
+            local status = config_table.Options.isBadgeAfterSpawnEnabled and "On" or "Off"
+            sampAddChatMessage("*** {FFFFFF}Badge After Spawn: {AAAAFF}" .. status, -1)
+        else
+            sampAddChatMessage("--- {AAAAFF}Badge After Spawn: {FFFFFF}Badge toggle in config failed - contact the developer for help.", -1)
+        end
     else
-        sampAddChatMessage("--- {AAAAFF}Badge After Spawn: {FFFFFF}Badge toggle in config failed - contact the developer for help.", -1)
+        sampAddChatMessage("--- {AAAAFF}Badge After Spawn: {FFFFFF}Config not loaded - contact the developer for help.", -1)
     end
 end
